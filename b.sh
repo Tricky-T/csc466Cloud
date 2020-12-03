8717d14b57c873a55f35269b08dc3b22714ed14a
Chin Cheuk Cheung <63791476+Tricky-T@users.noreply.github.com>
2020-12-03 12:37:20 -0500

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "export PATH=/opt/openmpi/3.1.2/bin/:$PATH" >> ~/.bashrc 
 echo "export LD_LIBRARY_PATH=/opt/openmpi/3.1.2/bin/:$LD_LIBRARY_PATH" >> ~/.bashrc
 sudo yum install -y nfs-utils
