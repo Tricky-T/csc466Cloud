c1821cec49fff3b8c7385a4af474fd717d0a8d54
Chin Cheuk Cheung <63791476+Tricky-T@users.noreply.github.com>
2020-12-03 12:35:51 -0500

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,6 @@
+echo "export PATH=/opt/openmpi/3.1.2/bin/:$PATH" >> ~/.bashrc 
+echo "export LD_LIBRARY_PATH=/opt/openmpi/3.1.2/bin/:$LD_LIBRARY_PATH" >> ~/.bashrc
+sudo yum install -y nfs-utils
+sudo mkdir -p /scratch
+sudo mount -t nfs 192.168.1.1:/scratch /scratch
+df -h
